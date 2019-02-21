786c8a83903ac97d0c533ff2c1c033d7371fbcf9
Ziyu Guo <zgguo@amazon.com>
2019-02-20 18:28:23 -0800

    Fix issue in output size calculation

--- python/dlr/api.py
+++ python/dlr/api.py
@@ -199,7 +199,7 @@ class DLRModel:
         dim : int
             The dimension of the index-th output.
         """
-        idx = ctypes.c_int()
+        idx = ctypes.c_int(index)
         size = ctypes.c_longlong()
         dim = ctypes.c_int()
         _check_call(_LIB.GetDLROutputSizeDim(byref(self.handle), idx,
@@ -220,7 +220,10 @@ class DLRModel:
             The shape of the index-th output.
         """
         size, dim = self._get_output_size_dim(index)
-        self.output_size_dim.append((size, dim))
+        if index >= len(self.output_size_dim):
+            self.output_size_dim.extend( [(0, 0)] * (index - len(self.output_size_dim) + 1))
+        print( self.output_size_dim)
+        self.output_size_dim[index] = (size, dim)
         shape = np.zeros(dim, dtype=np.int64)
         _check_call(_LIB.GetDLROutputShape(byref(self.handle),
                                                     c_int(index),
