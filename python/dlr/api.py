Fix issue in output size calculation (#26)
* Fix issue in output size calculation

python/dlr/api.py
@@ -199,7 +199,7 @@ class DLRModel:
         dim : int
             The dimension of the index-th output.
         """
-        idx = ctypes.c_int()
+        idx = ctypes.c_int(index)
         size = ctypes.c_longlong()
         dim = ctypes.c_int()
         _check_call(_LIB.GetDLROutputSizeDim(byref(self.handle), idx,
@@ -220,7 +220,9 @@ class DLRModel:
             The shape of the index-th output.
         """
         size, dim = self._get_output_size_dim(index)
-        self.output_size_dim.append((size, dim))
+        if not self.output_size_dim:
+            self.output_size_dim = [(0, 0)] * self._get_num_outputs()
+        self.output_size_dim[index] = (size, dim)
         shape = np.zeros(dim, dtype=np.int64)
         _check_call(_LIB.GetDLROutputShape(byref(self.handle),
                                                     c_int(index),

tests/python/integration/load_and_run_tvm_model.py
@@ -36,15 +36,18 @@ def test_multi_input_multi_output():
     device = 'cpu'
     model = DLRModel(model_path, device)
 
+    assert model._get_output_size_dim(0) == (2, 1)
+    assert model._get_output_size_dim(1) == (3, 1)
+
     input1 = np.asarray([1., 2.])
     input2 = np.asarray([3., 4.])
-    input3 = np.asarray([5., 6.])
-    input4 = np.asarray([7., 8.])
+    input3 = np.asarray([5., 6., 7])
+    input4 = np.asarray([8., 9., 10])
     input_map = {'data1': input1, 'data2': input2, 'data3': input3, 'data4': input4}
     outputs = model.run(input_map)
 
     assert outputs[0].tolist() == [4, 6]
-    assert outputs[1].tolist() == [12, 14]
+    assert outputs[1].tolist() == [13, 15, 17]
 
 
 if __name__ == '__main__':