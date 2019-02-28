430e7a225fb1337ee140688b116f117374cd2a4d
Philip Cho <chohyu01@cs.washington.edu>
2019-02-28 13:50:52 -0800

    Add GPU build

--- tests/ci_build/create_wheel.sh
+++ tests/ci_build/create_wheel.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+suffix="$1"
+
 cd python
 rm -f dist/*
 python setup.py bdist_wheel --universal
 for file in dist/*.whl
 do
-  mv ${file} ${file%-any.whl}-manylinux1_x86_64.whl
+  mv ${file} ${file%-any.whl}-${suffix}.whl
 done
