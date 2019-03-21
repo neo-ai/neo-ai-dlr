adf650dee57661180751b8e157ddab98ba71104c
ziyu-guo <40365354+ziyu-guo@users.noreply.github.com>
2019-03-21 12:28:59 -0700

    Update readme for pip (#36)
    * Update setup.py for pip

--- python/setup.py
+++ python/setup.py
@@ -42,4 +42,17 @@ setup(
     # include data files
     include_package_data=True,
     data_files=[('dlr', LIB_PATH)],
+
+    description = 'Common runtime for machine learning models compiled by \
+        AWS SageMaker Neo, TVM, or TreeLite.',
+    long_description=open(os.path.join(CURRENT_DIR, '../README.md')).read(),
+    author = 'AWS Neo',
+    author_email = 'aws-neo-ai@amazon.com',
+    url='https://github.com/neo-ai/neo-ai-dlr',
+    license = "Apache-2.0",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
 )
