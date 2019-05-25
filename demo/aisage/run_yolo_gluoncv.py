29aa1a72392b537f01e105c764d3e6a25c7863da
Alexander Pivovarov <pivovaa@amazon.com>
2019-05-24 23:06:03 -0700

    remove import relay

--- demo/aisage/run_yolo_gluoncv.py
+++ demo/aisage/run_yolo_gluoncv.py
@@ -1,20 +1,16 @@
 import tvm
 
 from matplotlib import pyplot as plt
-from tvm import relay
 from tvm.contrib import graph_runtime
 from gluoncv import data, utils
 
-voc_classes = =['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+voc_classes = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
 
-def run(graph, lib, params, ctx):
+def run(m, x, ctx):
     # Build TVM runtime
-    m = graph_runtime.create(graph, lib, ctx)
     tvm_input = tvm.nd.array(x.asnumpy(), ctx=ctx)
-    m.set_input('data', tvm_input)
-    m.set_input(**params)
     # execute
-    m.run()
+    m.run(data=tvm_input)
     # get outputs
     class_id = m.get_output(0)
     class_IDs, scores, bounding_boxs = m.get_output(0), m.get_output(1), m.get_output(2)
@@ -32,14 +28,17 @@ path_graph = "./models/yolov3_darknet53/deploy_graph.json"
 path_param = "./models/yolov3_darknet53/deploy_param.params"
 
 graph = open(path_graph).read()
-params = relay.load_param_dict(bytearray(open(path_param, "rb").read()))
+params = bytearray(open(path_param, "rb").read())
 lib = tvm.module.load(path_lib)
 
 dshape = (1, 3, 300, 300)
 dtype = "float32"
 
 ctx = tvm.opencl(0)
-class_IDs, scores, bounding_boxs = run(graph, lib, params, ctx)
+m = graph_runtime.create(graph, lib, ctx)
+m.load_params(params)
+
+class_IDs, scores, bounding_boxs = run(m, x, ctx)
 
 ######################################################################
 # Display result
@@ -47,4 +46,3 @@ class_IDs, scores, bounding_boxs = run(graph, lib, params, ctx)
 ax = utils.viz.plot_bbox(img, bounding_boxs.asnumpy()[0], scores.asnumpy()[0],
                          class_IDs.asnumpy()[0], class_names=voc_classes)
 plt.show()
-
