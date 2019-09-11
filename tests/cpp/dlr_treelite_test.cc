f8e22982852ca5430471a08a48bbe6047fff048b
Alexander Pivovarov <pivovaa@amazon.com>
2019-09-11 13:56:48 -0700

    Add tests/cpp (#85)

---
+++ tests/cpp/dlr_treelite_test.cc
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "dlr.h"
+
+
+TEST(Treelite, Test1) {
+  EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char ** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  testing::FLAGS_gtest_death_test_style = "threadsafe";
+  return RUN_ALL_TESTS();
+}
