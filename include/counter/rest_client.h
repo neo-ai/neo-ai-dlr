ec4d94561beb79aa209a15d75a3c9157a2f36763
dkasar-aws <dipak_kasar@persistent.co.in>
2020-01-14 17:02:01 +0530

    intermediate code

--- include/counter/rest_client.h
+++ include/counter/rest_client.h
@@ -6,7 +6,7 @@
 
 #if defined(__ANDROID__)
 #include <android/log.h>
-//#include <curl/curl.h>
+#include <curl/curl.h>
 #endif
 
 #include "config.h"
@@ -23,32 +23,57 @@ class RestClient {
      #if defined(__ANDROID__)
      #endif
   };
+
   void send(std::string data) {
-    std::cout << "Sending message " << data << std::endl; 
-     #if defined(__ANDROID__)
-    /* 
+    #if defined(__ANDROID__)
+    __android_log_print(ANDROID_LOG_DEBUG, "MyAPP", "inside restclient send %s ", data.c_str());
+     
     CURL *curl;
     CURLcode res;
     curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
-    #if defined(__ANDROID__)
-      //__android_log_print(ANDROID_LOG_DEBUG, "MyAPP", "inside restclient send %s ", data.c_str());
-    #endif
+    std::string readBuffer;
+ 
     if(curl) {
 
       struct curl_slist *headers=NULL;
-      headers=curl_slist_append(headers, "Content-Type: application/x-amz-json-1.1");
-      curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:5000/dlr_device_info");
-      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+      headers=curl_slist_append(headers, "Content-Type: application/x-amz-json-1.1; charset=UTF-8");
+      curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+ 
+      curl_easy_setopt(curl, CURLOPT_URL, CALL_HOME_URL.c_str());
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
+      char *s = curl_easy_escape(curl, data.c_str(), data.length());       
+      __android_log_print(ANDROID_LOG_DEBUG, "MyAPP", "New string =%s ", s);
+     
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, s);
+
       res = curl_easy_perform(curl);
-      if(res != CURLE_OK)
+        __android_log_print(ANDROID_LOG_DEBUG, "MyAPP", "restclient curl perform res =%d ", res);
+     
+       
+      if(res != CURLE_OK) {
+        __android_log_print(ANDROID_LOG_DEBUG, "MyAPP", "Error restclient curl perform ");
         fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
+                curl_easy_strerror(res));
+        __android_log_print(ANDROID_LOG_DEBUG, "MyAPP", "Error Msg =%s ",curl_easy_strerror(res) );
+      } else {
+        long resp_code;
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &resp_code);
+        __android_log_print(ANDROID_LOG_DEBUG, "MyAPP", "Resp CODE =%ld ",resp_code );
+        
+      } 
+
       curl_easy_cleanup(curl);
+      curl_slist_free_all(headers);
     }
+
+     
     curl_global_cleanup();
-    */
-     #endif
+    
+    #endif
   };
  private:
    
