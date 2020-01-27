13b16509789fc1877c651ba3bd6297dfa150b20a
Jason Wu <wuchih@amazon.com>
2020-01-27 10:25:09 -0800

    add capsys into integration test

--- tests/python/integration/test_counter_mgr.py
+++ tests/python/integration/test_counter_mgr.py
@@ -1,6 +1,6 @@
 import io
 import sys
-# import pytest
+import pytest
 
 from dlr import DLRModel
 import numpy as np
@@ -19,31 +19,41 @@ from test_utils import get_arch, get_models
 #     print(captured)
 #     assert captured.out == ''
 
-# TODO add integration to see the printer
 
-def test_notification():
-    # Load the model
+def test_notification(capsys):
+    # setup purpose
+    setup_mock_dlr()
+
+    # integration model mirror load_and_run_tvm_model.py
+    # load the model
     model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resnet18_v1')
     classes = 1000
     device = 'cpu'
     model = DLRModel(model_path, device)
 
-    # Run the model
+    # test the notification capture
+    captured = capsys.readouterr()
+    print(captured)
+    assert captured.out is not ''
+
+    # run the model
     image = np.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dog.npy')).astype(np.float32)
     # flatten within a input array
     input_data = {'data': image}
     print('Testing inference on resnet18...')
     probabilities = model.run(input_data)  # need to be a list of input arrays matching input names
+
     assert probabilities[0].argmax() == 151
 
 
-def setup():
+@pytest.fixture(scope="module")
+def setup_mock_dlr():
+    print("set up mock dlr")
+    
     arch = get_arch()
     model_names = ['resnet18_v1']
     for model_name in model_names:
         get_models(model_name, arch, kind='tvm')
-    test_notification()
-    print('All tests passed!')
 
-
-setup()
+    # test_notification()
+    # print('All tests passed!')
