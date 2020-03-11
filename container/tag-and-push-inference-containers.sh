bcaa0f3d48eb271a6b57bf875e701cd1186c3d56
Ashish Gupta <guas@amazon.com>
2020-03-11 16:30:22 -0700

    test

--- container/tag-and-push-inference-containers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source container/util.sh
-
-TARGET_REGIONS=($AWS_DEFAULT_REGION)
-TARGET_ACCOUNTS=($AWS_ACCOUNT_ID)
-
-tag_and_push $XGBOOST_REPO "XGBOOST_TAGS" "xgboost-cpu:latest" "TARGET_REGIONS" "TARGET_ACCOUNTS"
-tag_and_push $IMG_REPO "IMG_TAGS_CPU" "ic-cpu:latest" "TARGET_REGIONS" "TARGET_ACCOUNTS"
