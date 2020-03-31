c350875347f289480c330357fa0b68ea25926c25
wuchih@amazon.com <wuchih@amazon.com>
2020-03-31 16:58:55 -0700

    refactor test

--- tests/python/integration/test_load_counter_mgr.py
+++ tests/python/integration/test_load_counter_mgr.py
@@ -1,49 +1,53 @@
 from __future__ import print_function
+from dlr import DLRModel
 import numpy as np
 import os
 from test_utils import get_arch, get_models
+import time
 import cProfile
 
 
-def test_resnet():
-    from dlr import DLRModel
+def sample_data():
+    batch_size = 1
+    data_shape = (batch_size, 3, 224, 224)
+    data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
+    return data
 
+
+def load_test():
     # Load the model
     model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                               'resnet18_v1')
-    # classes = 1000
-    # device = 'cpu'
-    # model = DLRModel(model_path, device)
-    #
-    # # Run the model
-    # image = np.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dog.npy')).astype(np.float32)
-    # # flatten within a input array
-    # input_data = {'data': image}
-    # print('Testing inference on resnet18...')
-    # probabilities = model.run(input_data)  # need to be a list of input arrays matching input names
+    classes = 1000
+    device = 'cpu'
+    model = DLRModel(model_path, device)
 
-    for i in range(0, 10):
-        classes = 1000
-        device = 'cpu'
-        model = DLRModel(model_path, device)
-
+    results = []
+    for i in range(0, 100):
         # Run the model
         image = np.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dog.npy')).astype(np.float32)
         # flatten within a input array
         input_data = {'data': image}
         print('Testing inference on resnet18...')
+
+        start = time.time()
         probabilities = model.run(input_data)  # need to be a list of input arrays matching input names
+        end = time.time()
+        results.append(1000 * (end - start))
+
         assert probabilities[0].argmax() == 151
 
 
-def setup():
-    arch = get_arch()
+    results = np.array(results)
+    print(results.min(), results.mean(), results.max())
 
+
+def set_up():
+    arch = get_arch()
     model_names = ['resnet18_v1', '4in2out', 'assign_op']
     for model_name in model_names:
         get_models(model_name, arch, kind='tvm')
 
-
-setup()
-cProfile.run('test_resnet()', filename=None, sort=2)
-print('All tests passed!')
+set_up()
+cProfile.run('load_test()')
+print("test done")
