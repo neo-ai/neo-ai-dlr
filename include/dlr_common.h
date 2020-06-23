300990f57dd56d4bb6168bfe4d8e8af4e89c7ee1
Nag Varun Chunduru <cnv1989@gmail.com>
2020-06-23 08:41:54 -0700

    Use exceptions instead of return values to handle errors. Implemented unit tests. (#196)
    * Use exceptions instead of return values to handle errors. Implemented unit tests
    
    * Removed output_shapes_ from the attributes list
    
    * Added unit tests fro dlr
    
    Co-authored-by: varunnag <varunnag@amazon.com>

--- include/dlr_common.h
+++ include/dlr_common.h
@@ -91,46 +91,41 @@ class DLRModel {
   DLRModel(const DLContext& ctx, const DLRBackend& backend)
       : ctx_(ctx), backend_(backend) {}
   virtual ~DLRModel() {}
-  virtual void GetNumInputs(int* num_inputs) const {
-    *num_inputs = num_inputs_;
-  }
-  virtual void GetNumOutputs(int* num_outputs) const {
-    *num_outputs = num_outputs_;
-  }
-  virtual void GetNumWeights(int* num_weights) const {
-    *num_weights = num_weights_;
-  }
+
+  /* Input related functions */
+  virtual int GetNumInputs() const { return num_inputs_; }
   virtual const char* GetInputName(int index) const = 0;
   virtual const char* GetInputType(int index) const = 0;
-  virtual const char* GetWeightName(int index) const = 0;
-  virtual std::vector<std::string> GetWeightNames() const = 0;
   virtual void GetInput(const char* name, void* input) = 0;
-  virtual void SetInput(const char* name, const int64_t* shape, void* input,
-                        int dim) = 0;
-  virtual void Run() = 0;
-  virtual void GetOutput(int index, void* out) = 0;
+  virtual void SetInput(const char* name, const int64_t* shape, void* input, int dim) = 0;
+
+  /* Ouput related functions */
+  virtual int GetNumOutputs() { return num_outputs_; }
+  virtual const char* GetOutputName(const int index) const { 
+    LOG(ERROR) << "GetOutputName is not supported yet!";
+  }
+  virtual int GetOutputIndex(const char* name) const { 
+    LOG(ERROR) << "GetOutputName is not supported yet!";
+  }
+  virtual const char* GetOutputType(int index) const = 0;
   virtual void GetOutputShape(int index, int64_t* shape) const = 0;
   virtual void GetOutputSizeDim(int index, int64_t* size, int* dim) = 0;
-  virtual const char* GetOutputType(int index) const = 0;
+  virtual void GetOutput(int index, void* out) = 0;
+  virtual void GetOutputByName(const char* name, void* out) {
+    LOG(ERROR) << "GetOutputByName is not supported yet!";
+  }
+  
+  /* Weights releated functions */
+  virtual int GetNumWeights() const { return num_weights_; }
+  virtual const char* GetWeightName(int index) const = 0;
+  virtual std::vector<std::string> GetWeightNames() const = 0;
+  
   virtual const char* GetBackend() const = 0;
   virtual void SetNumThreads(int threads) = 0;
+  virtual bool HasMetadata() const {return false; }
   virtual void UseCPUAffinity(bool use) = 0;
-
-  virtual bool HasMetadata() const {
-    return false;
-  }
-
-  virtual const char* GetOutputName(const int index) const {
-    return nullptr;
-  }
-
-  virtual int GetOutputIndex(const char* name) const {
-    return -1;
-  }
-
-  virtual void GetOutputByName(const char* name, void* out) {
-    LOG(FATAL) << "GetOutputByName is not supported yet!";
-  }
+  virtual void Run() = 0;
+  
 };
 
 }  // namespace dlr
