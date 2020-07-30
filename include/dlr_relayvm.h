fc82c4e739f21a0819963db407db72d9ddb2ef11
Ubuntu <ubuntu@ip-172-31-29-195.us-west-2.compute.internal>
2020-07-30 23:58:02 +0000

    :qUpdated clan-format

--- include/dlr_relayvm.h
+++ include/dlr_relayvm.h
@@ -35,7 +35,7 @@ class RelayVMModel : public DLRModel {
   DLDataType GetInputDLDataType(int index);
 
  public:
-  explicit RelayVMModel(std::vector<std::string> paths, const DLContext &ctx)
+  explicit RelayVMModel(std::vector<std::string> paths, const DLContext& ctx)
       : DLRModel(ctx, DLRBackend::kRELAYVM) {
     InitModelPath(paths);
     LoadMetadata();
@@ -44,21 +44,20 @@ class RelayVMModel : public DLRModel {
     FetchOutputNodesData();
   }
 
-  int GetInputIndex(const char *name) const;
-  virtual const char *GetInputName(int index) const override;
-  virtual const char *GetInputType(int index) const override;
-  virtual const char *GetWeightName(int index) const override;
+  int GetInputIndex(const char* name) const;
+  virtual const char* GetInputName(int index) const override;
+  virtual const char* GetInputType(int index) const override;
+  virtual const char* GetWeightName(int index) const override;
   virtual std::vector<std::string> GetWeightNames() const override;
-  virtual void GetInput(const char *name, void *input) override;
-  virtual void SetInput(const char *name, const int64_t *shape, void *input,
-                        int dim) override;
+  virtual void GetInput(const char* name, void* input) override;
+  virtual void SetInput(const char* name, const int64_t* shape, void* input, int dim) override;
   virtual void Run() override;
   tvm::runtime::NDArray GetOutput(int index);
-  virtual void GetOutput(int index, void *out) override;
-  virtual void GetOutputShape(int index, int64_t *shape) const override;
-  virtual void GetOutputSizeDim(int index, int64_t *size, int *dim) override;
-  virtual const char *GetOutputType(int index) const override;
-  virtual const char *GetBackend() const override;
+  virtual void GetOutput(int index, void* out) override;
+  virtual void GetOutputShape(int index, int64_t* shape) const override;
+  virtual void GetOutputSizeDim(int index, int64_t* size, int* dim) override;
+  virtual const char* GetOutputType(int index) const override;
+  virtual const char* GetBackend() const override;
   virtual void SetNumThreads(int threads) override;
   virtual void UseCPUAffinity(bool use) override;
 
@@ -66,9 +65,9 @@ class RelayVMModel : public DLRModel {
     Following methods use metadata file to lookup input and output names.
   */
   virtual bool HasMetadata() const override;
-  virtual const char *GetOutputName(const int index) const override;
-  virtual int GetOutputIndex(const char *name) const override;
-  virtual void GetOutputByName(const char *name, void *out) override;
+  virtual const char* GetOutputName(const int index) const override;
+  virtual int GetOutputIndex(const char* name) const override;
+  virtual void GetOutputByName(const char* name, void* out) override;
 };
 
 }  // namespace dlr
--- tests/cpp/dlr_tvm_test.cc
+++ tests/cpp/dlr_tvm_test.cc
@@ -1,18 +1,14 @@
-#include <gtest/gtest.h>
 #include "dlr_tvm.h"
+#include <gtest/gtest.h>
 #include "test_utils.hpp"
-
 
 TEST(TVM, TestTvmModelApisWithOutputMetadata) {
   const int device_type = 1;  // 1 - kDLCPU
   const int device_id = 0;
-  DLContext ctx  = {
-    static_cast<DLDeviceType>(device_type),
-    device_id
-  };
+  DLContext ctx = {static_cast<DLDeviceType>(device_type), device_id};
   std::vector<std::string> paths = {"./resnet_v1_5_50"};
   dlr::TVMModel model = dlr::TVMModel(paths, ctx);
-  
+
   EXPECT_EQ(model.GetNumInputs(), 1);
   EXPECT_EQ(model.GetNumOutputs(), 2);
   EXPECT_EQ(model.HasMetadata(), true);
@@ -22,7 +18,7 @@ TEST(TVM, TestTvmModelApisWithOutputMetadata) {
   EXPECT_STREQ(model.GetOutputType(1), "float32");
 
   const int batch_size = 1;
-  size_t img_size = 224*224*3;
+  size_t img_size = 224 * 224 * 3;
   float* img = LoadImageAndPreprocess("cat224-3.txt", img_size, batch_size);
   img_size *= batch_size;
   int64_t shape[4] = {1, 224, 224, 3};
@@ -31,7 +27,7 @@ TEST(TVM, TestTvmModelApisWithOutputMetadata) {
   int output_dim;
   int index = 0;
   EXPECT_NO_THROW(model.GetOutputSizeDim(index, &output_size, &output_dim));
-  
+
   EXPECT_NO_THROW(model.SetInput("input_tensor", shape, img, 4));
   EXPECT_NO_THROW(model.Run());
 
@@ -43,25 +39,24 @@ TEST(TVM, TestTvmModelApisWithOutputMetadata) {
 
   try {
     model.GetOutputIndex("blah");
-  } catch(const dmlc::Error& e) {
+  } catch (const dmlc::Error& e) {
     EXPECT_STREQ(e.what(), "Couldn't find index for output node blah!");
   }
-  
+
   try {
     model.GetOutputByName("blah", output1);
-  } catch(const dmlc::Error& e) {
+  } catch (const dmlc::Error& e) {
     EXPECT_STREQ(e.what(), "Couldn't find index for output node blah!");
   }
 
   try {
     model.GetOutputName(2);
-  } catch(const dmlc::Error& e) {
+  } catch (const dmlc::Error& e) {
     EXPECT_STREQ(e.what(), "Output node with index 2 was not found in metadata file!");
   }
   delete[] output1;
   delete[] output2;
 }
-
 
 TEST(TVM, TestTvmModelApisWithoutMetadata) {
   std::string model_path = "./resnet_v1_5_50";
@@ -73,72 +68,64 @@ TEST(TVM, TestTvmModelApisWithoutMetadata) {
 
   const int device_type = 1;  // 1 - kDLCPU
   const int device_id = 0;
-  DLContext ctx  = {
-    static_cast<DLDeviceType>(device_type),
-    device_id
-  };
+  DLContext ctx = {static_cast<DLDeviceType>(device_type), device_id};
   std::vector<std::string> paths = {model_path};
   dlr::TVMModel model = dlr::TVMModel(paths, ctx);
-  
+
   EXPECT_EQ(model.GetNumInputs(), 1);
   EXPECT_EQ(model.GetNumOutputs(), 2);
   EXPECT_EQ(model.HasMetadata(), false);
   try {
     model.GetOutputName(0);
-  } catch(const dmlc::Error& e) {
+  } catch (const dmlc::Error& e) {
     EXPECT_STREQ(e.what(), "No metadata file was found!");
   }
   try {
     model.GetOutputIndex("blah");
-  } catch(const dmlc::Error& e) {
+  } catch (const dmlc::Error& e) {
     EXPECT_STREQ(e.what(), "No metadata file was found!");
   }
 
   // put it back
   std::rename(metadata_file_bak.c_str(), metadata_file.c_str());
 }
-
 
 TEST(TVM, TestTvmModelApisWithoutOutputInMetadata) {
   // rename metadata file such that metadata file is not found.
   std::string model_path = "./resnet_v1_5_50";
   std::string metadata_file = model_path + "/compiled.meta";
   std::string metadata_file_bak = model_path + "/compiled.meta.bak";
-  
+
   std::rename(metadata_file.c_str(), metadata_file_bak.c_str());
   std::ofstream mocked_metadata;
   mocked_metadata.open(metadata_file.c_str());
   mocked_metadata << "{}\n";
   mocked_metadata.close();
-  
+
   const int device_type = 1;  // 1 - kDLCPU
   const int device_id = 0;
-  DLContext ctx  = {
-    static_cast<DLDeviceType>(device_type),
-    device_id
-  };
+  DLContext ctx = {static_cast<DLDeviceType>(device_type), device_id};
   std::vector<std::string> paths = {model_path};
   dlr::TVMModel model = dlr::TVMModel(paths, ctx);
-  
+
   EXPECT_EQ(model.GetNumInputs(), 1);
   EXPECT_EQ(model.GetNumOutputs(), 2);
   EXPECT_EQ(model.HasMetadata(), true);
 
   try {
     model.GetOutputName(0);
-  } catch(const dmlc::Error& e) {
+  } catch (const dmlc::Error& e) {
     EXPECT_STREQ(e.what(), "Output node with index 0 was not found in metadata file!");
   }
   try {
     model.GetOutputIndex("blah");
-  } catch(const dmlc::Error& e) {
+  } catch (const dmlc::Error& e) {
     EXPECT_STREQ(e.what(), "Output node with index 0 was not found in metadata file!");
   }
 
   // put it back
   std::rename(metadata_file_bak.c_str(), metadata_file.c_str());
 }
-
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
