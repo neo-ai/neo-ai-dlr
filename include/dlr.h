cfb2a06448bf2247e30ef8db9350e7ee764b0348
Trevor Morris <trevmorr@amazon.com>
2020-09-16 17:15:30 +0000

    Release 1.3.1

--- include/dlr.h
+++ include/dlr.h
@@ -19,7 +19,7 @@ extern "C" {  // Open extern "C" block
 /*! \brief minor version */
 #define DLR_MINOR 3
 /*! \brief patch version */
-#define DLR_PATCH 0
+#define DLR_PATCH 1
 /*! \brief DLR version */
 #define DLR_VERSION (DLR_MAJOR * 10000 + DLR_MINOR * 100 + DLR_PATCH)
 /*! \brief helper for making version number */
