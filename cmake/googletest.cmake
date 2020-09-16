2bb5723e48f8323e2c8d1f9c6d38d6e1bf71a847
wuchih@amazon.com <wuchih@amazon.com>
2020-09-16 15:46:17 -0700

    Revert "implement installation of neodlr library (#228)"
    This reverts commit 52d2aa12630fa6e8b955ef27892b6144eaeb5fd4.

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -4,7 +4,6 @@
 
 macro(fetch_googletest _download_module_path _download_root)
     set(GOOGLETEST_DOWNLOAD_ROOT ${_download_root})
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
     configure_file(
         ${_download_module_path}/googletest-download.cmake
         ${_download_root}/CMakeLists.txt
