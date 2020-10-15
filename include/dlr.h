68d48131ec3fa213178ce327452f3cbb0680ba02
Alexander Pivovarov <pivovaa@amazon.com>
2020-10-14 17:42:44 -0700

    Add CreateDLRPipeline

--- include/dlr.h
+++ include/dlr.h
@@ -124,6 +124,22 @@ int CreateDLRModelFromHexagon(DLRModelHandle* handle, const char* model_path,
 #endif  // DLR_HEXAGON
 
 /*!
+ \brief Creates a DLR pipeline model.
+ \param handle The pointer to save the model handle.
+ \param num_models Number of items in model_paths array
+ \param model_paths Paths to the folders containing the models files,
+                    or colon-separated list of folders (or files) if model files
+                    stored in different locations
+ \param dev_type Device type. Valid values are in the DLDeviceType enum in dlpack.h.
+ \param dev_id Device ID.
+ \return 0 for success, -1 for error. Call DLRGetLastError() to get the error message.
+ */
+DLR_DLL
+int CreateDLRPipeline(DLRModelHandle* handle,
+                      int num_models, const char** model_paths,
+                      int dev_type, int dev_id);
+
+/*!
  \brief Deletes a DLR model.
  \param handle The model handle returned from CreateDLRModel().
  \return 0 for success, -1 for error. Call DLRGetLastError() to get the error
