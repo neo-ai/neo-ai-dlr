7818a6c6742fa2514a5a6aa4c3ced7f24a3d2c7c
Ubuntu <ubuntu@ip-172-31-6-220.us-west-2.compute.internal>
2020-11-11 21:30:25 +0000

    fixed lint

--- include/dlr_hexagon/dlr_hexagon.h
+++ include/dlr_hexagon/dlr_hexagon.h
@@ -46,7 +46,8 @@ class HexagonModel : public DLRModel {
  public:
   /*! \brief Load model files from given folder path.
    */
-  explicit HexagonModel(const std::vector<std::string>& files, const DLContext& ctx, const int debug_level);
+  explicit HexagonModel(const std::vector<std::string>& files, const DLContext& ctx,
+                        const int debug_level);
   ~HexagonModel();
 
   virtual const int GetInputDim(int index) const override;
