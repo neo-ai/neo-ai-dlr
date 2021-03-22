c00d021a652752e4723ffd769e6ffc5715772ed6
mayunha <yunma10@outlook.com>
2021-03-22 17:50:40 +0000

    remove local time dependent test

--- tests/cpp/dlr_data_transform_test.cc
+++ tests/cpp/dlr_data_transform_test.cc
@@ -198,19 +198,18 @@ TEST(DLR, DataTransformDateTime) {
   EXPECT_TRUE(transform.HasInputTransform(metadata));
 
   data =
-      R"([["Feb 11th, 2012, 11:34:59pm", "2006-08-23"], ["2017-05-08 14:21:28", ""], ["12:28:48.000001", "12:28:48.000001+00"], ["2004-09-07 12:28:48.000001", "2004-09-07 12:28:48.000001+00"]])";
+      R"([["Feb 11th, 2012, 11:34:59pm", "2006-08-23"], ["2017-05-08 14:21:28", ""], ["2004-09-07 12:28:48.000001", "2004-09-07 12:28:48.000001+00"]])";
   shape = {static_cast<int64_t>(std::strlen(data))};
 
   EXPECT_NO_THROW(transform.TransformInput(metadata, shape.data(), const_cast<char*>(data),
                                            shape.size(), dtypes, ctx, &transformed_data));
 
   EXPECT_EQ(transformed_data[0]->ndim, 2);
-  EXPECT_EQ(transformed_data[0]->shape[0], 4);
+  EXPECT_EQ(transformed_data[0]->shape[0], 3);
   EXPECT_EQ(transformed_data[0]->shape[1], 14);
 
   expected_output = {6, 2012, 23, 34, 59, 2, 6,  3, 2006, 0,  0,  0,  8, 34,
                      1, 2017, 14, 21, 28, 5, 19, 0, 1900, 0,  0,  0,  1, 52,
-                     5, 2021, 12, 28, 48, 3, 11, 5, 2021, 12, 28, 48, 3, 11,
                      2, 2004, 12, 28, 48, 9, 37, 2, 2004, 12, 28, 48, 9, 37};
 
   for (size_t i = 0; i < expected_output.size(); ++i) {
